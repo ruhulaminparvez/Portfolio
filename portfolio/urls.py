05b51c819d774b4164336e512874e5b15b97a185
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-04-07 00:06:41 +0600

    Complete Project

---
+++ portfolio/urls.py
@@ -0,0 +1,41 @@
+"""portfolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+# from django.conf.urls import url
+admin.site.site_header = "My Portfolio Admin"
+admin.site.site_title = "My Portfolio Admin Portal"
+admin.site.index_title = "Welcome to My Portfolio Admin Panel"
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace="main")),
+    # url(r'^media/(?P<path>.*)$', serve,
+    #     {'document_root': settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,
+    #     {'document_root': settings.STATIC_ROOT}),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
